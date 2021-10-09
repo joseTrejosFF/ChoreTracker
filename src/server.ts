77dcb16aaa6d12a324ed8e381d1ee42ffc725f07
Jose Trejos <jtrejos506@gmail.com>
2021-10-09 08:58:17 -0600

    deploy-heroku

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express";
 import connectDB from "./config/db";
+import path from "path";
 
 const app = express();
 
@@ -7,6 +8,14 @@ const app = express();
 connectDB();
 
 app.use("/api/chores", require("./routes/chores"));
+
+// static folder
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 const PORT = process.env.PORT || 3333;
 
